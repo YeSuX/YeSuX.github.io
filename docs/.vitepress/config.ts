b35458412c993d91c8c67a230daf8ba6be3fccd0
叶苏雄 <xiesuxiong@xiesuxiongdeMBP.lan>
2022-10-28 22:24:37 +0800

    feat: project set up

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,4 @@
+export default {
+  title: 'suxioing blog',
+  description: 'Just playing around.',
+};
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import UnoCSS from 'unocss/vite';
-import Icons from 'unplugin-icons/vite';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [UnoCSS(), vue()],
-});
