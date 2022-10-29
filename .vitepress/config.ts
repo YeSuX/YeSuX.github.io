774edc8e29b327727f15ed2f5f43a47ce2ae0cfd
叶苏雄 <xiesuxiong@xiesuxiongdeMBP.lan>
2022-10-29 10:02:34 +0800

    feat: 项目框架搭建

---
+++ .vitepress/config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  lang: 'en-US',
+  title: 'suxiong',
+  description: "suxiong's blog",
+  lastUpdated: true,
+  cleanUrls: 'without-subfolders',
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Blog', link: '/blog/' },
+      { text: 'Projects', link: '/projects/' },
+      { text: 'Talks', link: '/talks/' },
+      { text: 'Podcats', link: '/podcats/' },
+      { text: 'Streams', link: '/streams/' },
+      { text: 'Demos', link: '/demos/' },
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/YeSuX' },
+      { icon: 'twitter', link: 'https://twitter.com/YeSuX1998' },
+      {
+        icon: 'linkedin',
+        link: 'https://www.linkedin.com/in/ye-suxiong-b4b080228/',
+      },
+      { icon: 'discord', link: 'https://discord.gg/evZrD9h5qW' },
+    ],
+    footer: {
+      message: 'Light tomorrow with today.',
+      copyright: '2022-present © suxiong',
+    },
+    lastUpdatedText: 'Updated Date',
+  },
+});
--- docs/.vitepress/config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  title: 'suxioing blog',
-  description: 'Just playing around.',
-};
